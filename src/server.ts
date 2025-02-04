0a03ba5f625a99f72e1cc721a3489f903e7f8fa2
Deepak-Das-7 <deepak.d@feniceenergy.com>
2025-02-04 14:42:21 +0530

    sadsa

--- src/server.ts
+++ src/server.ts
@@ -17,15 +17,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
-app.use(cors(corsOptions));
 
 
 // Middleware
